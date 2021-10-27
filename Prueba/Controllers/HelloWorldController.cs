a7fe8bd0edf9a4be5a7252bef5095c350618f157
Yasmin <neresdesouza.yasmin@gmail.com>
2021-10-27 13:39:52 +0200

    Agregar Controlador

---
+++ Prueba/Controllers/HelloWorldController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public string Index()
+        {
+            return "Hola Yasmin";
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+
+        // Pasamos como parametros 2 variables y le llamaremos en la URL
+        /*public string Welcome( string nombre , int edad=1)
+        {
+            return HtmlEncoder.Default.Encode($"Hola {nombre}, tu edad es : {edad}");
+        }*/
+
+        public string Welcome(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hola {name}, ID: {ID}");
+        }
+
+    }
+}
